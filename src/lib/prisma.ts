88db981842a72a870c0f2cfc2b3860dccbf7e4e4
gjulia <julia.gari75@ynov.com>
2026-07-15 15:59:11 +0200

    fix: restauration du client Prisma (fichier ecrase par le code axios du client)

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,29 +1,5 @@
-import axios from "axios";
+import { PrismaClient } from "@prisma/client";
 
-const api = axios.create({
-    baseURL: "http://localhost:4000/api",
-});
+const prisma = new PrismaClient();
 
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
-
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            localStorage.removeItem("token");
-            localStorage.removeItem("user");
-            if (window.location.pathname !== "/login") {
-                window.location.href = "/login";
-            }
-        }
-        return Promise.reject(error);
-    }
-);
-
-export default api;
+export default prisma;
